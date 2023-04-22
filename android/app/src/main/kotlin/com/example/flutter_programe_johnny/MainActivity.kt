b069de78033d83e37a5850459d89efc08042ca8e
Johnny <h160799@gmail.com>
2023-04-22 14:05:12 +0800

    Platform Channel

--- android/app/src/main/kotlin/com/example/flutter_programe_johnny/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_programe_johnny/MainActivity.kt
@@ -1,6 +1,25 @@
 package com.example.flutter_programe_johnny
 
+import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
 class MainActivity: FlutterActivity() {
+    private val CHANNEL = "com.example.flutter_programe_johnny"
+
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+    super.configureFlutterEngine(flutterEngine)
+    MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler{
+      // This method is invoked on the main thread.
+      call, result ->
+      if (call.method == "callAndroid") {
+        // 處理 callAndroid 方法的請求
+            val response = "Hello from Android"
+            result.success(response)
+        } else {
+        result.notImplemented()
+      }
+    }
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_programe_johnny/pages/home_page.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 void main() => runApp(const MyApp());
 
@@ -8,6 +10,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    PlatformChannel _platformChannel = PlatformChannel();
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
@@ -16,12 +20,28 @@ class MyApp extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               SizedBox(
-                width: 120.0, // 設置圖像寬度
-                child: Image.asset(
-                  'assets/images/stylish_Logo.png',
-                  fit: BoxFit.fitWidth,
-                ),
-              ),
+                  width: 120.0, // 設置圖像寬度
+                  child: InkWell(
+                    onTap: () async {
+                      String result = await _platformChannel.callAndroid();
+                      print(result);
+
+                      Fluttertoast.showToast(
+                          msg: result,
+                          toastLength: Toast.LENGTH_SHORT,
+                          gravity: ToastGravity.BOTTOM,
+                          timeInSecForIosWeb: 1,
+                          backgroundColor: Colors.red,
+                          textColor: Colors.white,
+                          fontSize: 16.0);
+
+                      // Success: Hello from Android
+                    },
+                    child: Image.asset(
+                      'assets/images/stylish_Logo.png',
+                      fit: BoxFit.fitWidth,
+                    ),
+                  )),
             ],
           ),
           backgroundColor: Colors.grey[200],
@@ -29,5 +49,20 @@ class MyApp extends StatelessWidget {
         body: const HomePage(),
       ),
     );
+  }
+}
+
+class PlatformChannel {
+  static const platform = MethodChannel('com.example.flutter_programe_johnny');
+
+  Future<String> callAndroid() async {
+    String result = "";
+    try {
+      final String response = await platform.invokeMethod('callAndroid');
+      result = "Success: $response";
+    } on PlatformException catch (e) {
+      result = "Failed: ${e.message}";
+    }
+    return result;
   }
 }
